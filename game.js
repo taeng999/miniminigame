d595b9f68f99b0f4ff4359db362d8e3a590dd15f
Park June Chul <pjc0247@naver.com>
2016-07-26 11:17:14 +0900

    Update game.js

--- game.js
+++ game.js
@@ -33,6 +33,26 @@ var MyScene = cc.Scene.extend({
         cc.MoveBy.create(0.02, cc.p(6,0)),
         cc.MoveBy.create(0.03, cc.p(-3,0))
       ));
+
+    if (Math.random() > 0.5) {
+      var x = Math.random() * 70 - 35;
+      var y = Math.random() * 50 - 25;
+
+      item = cc.Sprite.create("apple_1.png");
+      item.setPosition(cc.p(200 + x, 200 + y));
+      item.setOpacity(0);
+      item.runAction(
+        cc.EaseBackIn.create(
+        cc.MoveBy.create(0.5, cc.p(0,-80 - y))));
+      item.runAction(
+        cc.Sequence.create(
+          cc.FadeTo.create(0.2, 255),
+          cc.DelayTime.create(0.5),
+          cc.FadeTo.create(0.2, 0),
+          cc.RemoveSelf.create()
+        ));
+      _this.addChild(item);
+    }
   },
   onMouseUp:function(e) {
   },
